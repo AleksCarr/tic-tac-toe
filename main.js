9b195daf31d8fa3bd777d8dd5ebca50ec54bde71
Aleksander Carr <aleks.carr@outlook.com>
2020-11-11 14:26:14 -0500

    made it so the x and o images are hidden on start.

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 
+document.querySelectorAll(".O-Cell4")[0].style.display = "none";
+document.querySelectorAll(".X-Cell4")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell1")[0].style.display = "none";
+document.querySelectorAll(".X-Cell1")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell2")[0].style.display = "none";
+document.querySelectorAll(".X-Cell2")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell3")[0].style.display = "none";
+document.querySelectorAll(".X-Cell3")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell5")[0].style.display = "none";
+document.querySelectorAll(".X-Cell5")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell6")[0].style.display = "none";
+document.querySelectorAll(".X-Cell6")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell7")[0].style.display = "none";
+document.querySelectorAll(".X-Cell7")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell8")[0].style.display = "none";
+document.querySelectorAll(".X-Cell8")[0].style.display = "none";
+
+document.querySelectorAll(".O-Cell9")[0].style.display = "none";
+document.querySelectorAll(".X-Cell9")[0].style.display = "none";
